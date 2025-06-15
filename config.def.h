c16a83605f71706690b15415c69cb0a8f2cd2499
Amr <amr@programmer.net>
2025-06-15 13:17:29 +0300

    use case insensitive for dmenu

--- config.def.h
+++ config.def.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_accent, "-sf", col_fg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_accent, "-sf", col_fg, NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
